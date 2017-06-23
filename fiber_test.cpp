6dc3e019d56700143254055ccd4b96ae5400ae5d
zodd-pl <none>
2017-06-24 00:30:13 +0200

    fix crash on fiber join by changing /MTd to /MDd

--- fiber_test.cpp
+++ fiber_test.cpp
@@ -1,6 +1,8 @@
 
-#include <boost/fiber/all.hpp>
+#include  <memory>
 
+#include <boost/fiber/all.hpp>
+#include <boost/fiber/protected_fixedsize_stack.hpp>
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
@@ -10,17 +12,19 @@ namespace fibers = boost::fibers;
 
 TEST( fiber_basic, test )
 {
-	auto func = [] 
 	{
-		std::cout << "fiber: " << boost::this_fiber::get_id() << " enter" << std::endl;
-		//boost::this_fiber::yield();
-		std::cout << "fiber: " << boost::this_fiber::get_id() << " exit" << std::endl;
-	};
+		auto func = []
+		{
+			std::cout << "fiber: " << boost::this_fiber::get_id() << " enter" << std::endl;
+			boost::this_fiber::yield();
+			std::cout << "fiber: " << boost::this_fiber::get_id() << " exit" << std::endl;
+		};
 
-	fibers::fiber f1(func);
-	//fibers::fiber f2(func);
+		fibers::fiber f1(func);
+		fibers::fiber f2(func);
 
-	f1.join();
-	//f2.join();
+		f1.join();
+		f2.join();
+	}
 }
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+extern void dupa();
 
 int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
+
+
