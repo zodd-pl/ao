650f91c824ea6893677f679d225c916ec6211bc6
zodd-pl <none>
2017-06-23 23:17:39 +0200

    enable debuging

--- fiber_test.cpp
+++ fiber_test.cpp
@@ -5,12 +5,22 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
-
+namespace fibers = boost::fibers;
 
 
 TEST( fiber_basic, test )
 {
+	auto func = [] 
+	{
+		std::cout << "fiber: " << boost::this_fiber::get_id() << " enter" << std::endl;
+		//boost::this_fiber::yield();
+		std::cout << "fiber: " << boost::this_fiber::get_id() << " exit" << std::endl;
+	};
 
+	fibers::fiber f1(func);
+	//fibers::fiber f2(func);
 
+	f1.join();
+	//f2.join();
 }
 
